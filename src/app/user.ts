2fe23280ba4218eeba15d90b794284acae08dd2f
Oliver <muriithioliver11@outlook.com>
2019-07-15 18:03:31 +0300

    Will edit user class

--- src/app/user.ts
+++ src/app/user.ts
@@ -2,8 +2,7 @@ export class User {
     name: string;
     login: string;
     avatar_url: string;
-    followers: number;
-    following: number;
+    
     html_url: string;
     public_repos: number;
 }
