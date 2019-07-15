17afa006c75a938f4beeba382251f2c6383c5b08
Oliver <muriithioliver11@outlook.com>
2019-07-15 18:03:49 +0300

    Will add followers and following in user class

--- src/app/user.ts
+++ src/app/user.ts
@@ -2,7 +2,8 @@ export class User {
     name: string;
     login: string;
     avatar_url: string;
-    
+    followers: number;
+    following: number;
     html_url: string;
     public_repos: number;
 }
